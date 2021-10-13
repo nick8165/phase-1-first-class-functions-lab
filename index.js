6bb83ccb84cb3fb145c16ec5520cac5aaef0d30f
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-10-13 15:58:30 -0500

    commit tree e9ef30a73f31ac5d561af7f0eee93c23dc859d8d

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers() {
+    return firstTwo;
+}
+
+function returnLastTwoDrivers() {
+    return lastTwo;
+}
+
+function createFareMultiplier(integer) {
+        const fare = integer;
+    return function fareQuintupler(fare) {
+        return fare * 5;
+    }
+}
+
+function fareDoubler(fare) {
+    const doubledFare = fare * 2;
+    return doubledFare;
+}
+
+function fareTripler(fare) {
+    const tripledFare = fare * 3;
+    return tripledFare;
+}
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    if (selectingDrivers === firstTwo) {
+        return firstTwo;
+    } else if (selectingDrivers === lastTwo) {
+        return lastTwo;
+    }
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; 
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const firstTwo = drivers.slice(0, 2);
+const lastTwo = drivers.slice(-2);
+console.log(fareDoubler(10));
